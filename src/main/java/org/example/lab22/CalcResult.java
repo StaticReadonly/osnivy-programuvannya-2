b2ac1720eb32e946685944dd927ff611bae4b196
StaticReadonly <klymukvlad4002@gmail.com>
2024-02-17 18:07:57 +0200

    vlad-form ready

---
+++ src/main/java/org/example/lab22/CalcResult.java
@@ -0,0 +1,9 @@
+package org.example.lab22;
+
+public class CalcResult {
+    public double a = 0;
+    public double b = 0;
+    public double c = 0;
+    public double d = 0;
+    public double result = 0;
+}
---
+++ src/main/java/org/example/lab22/Defaults.java
@@ -0,0 +1,19 @@
+package org.example.lab22;
+
+public class Defaults {
+    public static final String _parameterA = "a";
+    public static final String _parameterB = "b";
+    public static final String _parameterC = "c";
+    public static final String _parameterD = "d";
+    public static final String _parameterRes = "res";
+    public static final  String _resultsKey = "Results";
+    public static final String _jspFile = "vlad-form.jsp";
+
+    public static class Errors{
+        public static final String _errorParameter = "Error";
+        public static final String _zeroDivisionError = "Can't divide by 0!";
+        public static final String _invalidFormData = "Invalid form data!";
+        public static final String _invalidKey = "Invalid key detected!";
+        public static final String _logFromZero = "Log from 0 does not exist!";
+    }
+}
